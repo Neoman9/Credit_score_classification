6e326f726b085e08da1bf3c77a8e5762f3d7fd67
Neoman9 <nelsonbenedict35@yahoo.com>
2023-10-08 18:50:49 +0100

    schema written

---
+++ src/credit_classification/components/data_validation.py
@@ -0,0 +1,2 @@
+from src.credit_classification.logger  import logging
+from src.cre
--- src/credit_classification/entity/artifact_entity.py
+++ src/credit_classification/entity/artifact_entity.py
@@ -2,4 +2,4 @@ from collections import namedtuple
 
 DataIngestionArtifact= namedtuple("DataIngestionArtifact", ["train_file_path", "test_file_path", "is_ingested", "ingestion"])
 
-DataValidationArtifact= namedtuple("DataValidationArtifact", ["schema_file_path", "is_validated", "message"], "status_file_path")
+DataValidationArtifact= namedtuple("DataValidationArtifact", ["schema_file_path", "is_validated", "message", "status_file_path"] )
